99361c1372d9f2e69660884888e1a59e72f736d9
Isaac Negatu <isaac.negatu@gmail.com>
2018-07-18 22:13:37 -0500

    sql databse created

---
+++ nunki_music.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE "nunki_music";
+
+CREATE TABLE "artist"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(255) NOT NULL,
+	"image_url" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "track"(
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(255) NOT NULL,
+	"image_url" VARCHAR(255) NOT NULL,
+	"api_url" VARCHAR(255) NOT NULL,
+	"artist_id" INT REFERENCES "artist"
+);
+
+CREATE TABLE "playlist"(
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "track_playlist"(
+	"track_id" INT NOT NULL REFERENCES "track",
+	"playlist_id" INT NOT NULL REFERENCES "playlist"
+);
